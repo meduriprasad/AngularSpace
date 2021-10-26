102ff89e3631b096da98104a68fdb1099dd56a6a
meduriprasad <meduriprasad@gmail.com>
2021-10-26 12:06:10 -0500

    changes

--- angularhttp/src/app/app.component.ts
+++ angularhttp/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { UserService } from './service/user.service';
 import { Component, OnInit } from '@angular/core';
 import { User } from './interface/user';
+import { HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,43 +9,52 @@ import { User } from './interface/user';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
+  
+  fileStatus = { status: '', percentage: 0 };
   title = 'angularhttp';
-  private user: User = {
-    'name': 'Test Graham',
-    'username': 'TEST',
-    'email': 'Sincere@april.biz',
-    'address': {
-    'street': 'Kulas Light',
-    'suite': 'Apt. 556',
-    'city': 'Gwenborough',
-    'zipcode': '92998-3874',
-    'geo': {
-    'lat': '-37.3159',
-    'lng': '81.1496'
-    }
-    },
-    'phone': '1-770-736-8031 x56442',
-    'website': 'hildegard.org',
-    'company': {
-    'name': 'Romaguera-Crona',
-    'catchPhrase': 'Multi-layered client-server neural-net',
-    'bs': 'harness real-time e-markets'
-    }
-    }
+  
+  private user: any = {
+    'id': 2,
+    'name': 'Junior Graham',
+    'username': 'junior',
+    'email': 'junior@april.biz'
+  };
+
+    private updateUser: User = {
+      'id': 5,
+      'name': 'Test1 Graham',
+      'username': 'TEST1',
+      'email': 'Sincere@april.biz',
+      'phone': '1-770-736-8031 x56442',
+      'website': 'hildegard.org',
+      }
+
+      private patchUser: any = {
+        'id': 5,
+        'name': 'Patch Graham',
+        'username': 'Patch',
+        }
+        users: User[] | undefined;
   constructor(private userService: UserService){
   }
 
   ngOnInit(): void {
-    this.onCreateUser();
+    // this.onCreateUser();
     this.onGetUsers();
-    this.onGetUser();
+    // this.onGetUser();
+    // this.onUpdateUser();
+    // this.onPatchUser();
+    // this.onDeleteUser();
   }
 
   onGetUsers(): void{
     this.userService.getUsers().subscribe(
-      (response) => console.table(response),
+      (response) => {
+        console.log(response);
+        this.users = response
+      },
       (error: any) => console.log(error),
-      () => console.log('Done getting the data')
+      () => console.log('Done getting users')
     );
   }
 
@@ -64,4 +74,73 @@ export class AppComponent implements OnInit{
     );
   }
 
+  onUpdateUser(): void{
+    this.userService.updateUser(this.updateUser).subscribe(
+      (response) => console.log(response),
+      (error: any) => console.log(error),
+      () => console.log('Done updating the user')
+    );
+  }
+
+  onPatchUser(): void{
+    this.userService.patchUser(this.patchUser).subscribe(
+      (response) => console.log(response),
+      (error: any) => console.log(error),
+      () => console.log('Done patching the user')
+    );
+  }
+
+  onDeleteUser(): void{
+    this.userService.deleteUser(5).subscribe(
+      (response) => console.log(response),
+      (error: any) => console.log(error),
+      () => console.log('Done deleting the user')
+    );
+  }
+  onTextFile(): void {
+    this.userService.getTextFile().subscribe(
+      (response) => console.log('Response: ', response),
+      (error: any) => console.log(error),
+      () => console.log('Done getting text file')
+    );
+  } 
+
+  onUploadFile(files: File[]): void {
+    console.log(files);
+    const formData = new FormData();
+        for (const file of files) {
+            formData.append('files', file, file.name);
+        }
+    this.userService.uploadFiles(formData).subscribe(
+      (event) => {
+        switch (event.type) {
+        case HttpEventType.UploadProgress || HttpEventType.DownloadProgress:
+          console.log(event);
+          this.fileStatus.percentage = Math.round(100 * event.loaded / event.loaded);
+          this.fileStatus.status = 'progress';
+          console.log(this.fileStatus);
+          break;
+        case HttpEventType.Response:
+          console.log(event);
+          if (event.status === 200) {
+            console.log(event);
+            this.fileStatus.status = 'done';
+            this.fileStatus.percentage = 0;
+            console.log(this.fileStatus);
+            break;
+          } else {
+            console.log(event);
+            this.fileStatus.status = 'done';
+            this.fileStatus.percentage = 0;
+            break;
+          }
+        default:
+        // Clean up here;
+      }
+        
+      },
+      (error: any) => console.log(error),
+      () => console.log('Done uploading files')
+    );
+  }
 }
--- angularhttp/src/app/service/user.service.ts
+++ angularhttp/src/app/service/user.service.ts
@@ -1,29 +1,95 @@
 import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpEvent, HttpHeaders, HttpResponse } from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { User } from '../interface/user';
+import { catchError, retry, tap, map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  readonly moreParams = ['test1', 'test2'];
+  private imageUrl = 'https://robohash.org';
   private apiUrl= environment.apiUrl
 
   constructor(private http: HttpClient) { }
 
   getUsers(): Observable<User[]>{
-    
     return this.http.get<User[]>(`${this.apiUrl}/users`)
+    .pipe(
+      map(users => 
+            users.map(user => ({
+              ...user,
+              //name: user.name.toUpperCase(),
+              defaultImageUrl: `https://robohash.org/${user.username}`.toLowerCase(),
+              searchKey: [user.name, user.username],
+              role: user.id === 10? 'admin': 'user'
+            })))
+    );
+  }
+
+  getUsers2(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.apiUrl}/users`)
+    .pipe(
+      map(users => 
+            users.map(user => ({
+              id: user.id,
+              username: user.username,
+              phone: user.phone,
+              email: user.email,
+              website: user.website,
+              name: user.name.toUpperCase(),
+              defaulImageUrl: this.imageUrl,
+              searchKey: [user.name, user.username],
+              isAdmin: user.id === 100
+            })))
+    );
+  }
+  getAllUsers(): Observable<HttpEvent<User[]>>{
+    
+    return this.http.get<User[]>(`${this.apiUrl}/users`,{observe: 'events'})
   }
 
   getUser(): Observable<User>{
-    
     return this.http.get<User>(`${this.apiUrl}/users/1`)
+  .pipe(
+    map(user => { 
+          return { ...user, isAdmin: false } 
+        })
+  );
   }
 
   createUser(user: User): Observable<User>{
     
     return this.http.post<User>(`${this.apiUrl}/users`, user)
+  }
+
+  updateUser(user: User): Observable<User>{
+    
+    return this.http.put<User>(`${this.apiUrl}/users/${user.id}`, user)
+  }
+
+  patchUser(user: User): Observable<User>{
+    
+    return this.http.patch<User>(`${this.apiUrl}/users/${user.id}`, user)
+  }
+
+  deleteUser(id: number): Observable<boolean>{
+    
+    return this.http.delete<boolean>(`${this.apiUrl}/users/${id}`)
+  }
+
+  getTextFile(): Observable<string> {
+    return this.http.get(`assets/text.txt`, { responseType: 'text'});
+  }
+
+  downloadFile(): Observable<HttpResponse<Blob>> {
+    return this.http.get(`assets/text.txt`, { responseType: 'blob', observe: 'response'});
+  }
+
+  uploadFiles(formData: FormData): Observable<HttpEvent<string[]>> {
+    return this.http.post<string[]>(`http://localhost:9000/file/upload`, formData,
+    { observe: 'events', reportProgress: true });
   }
 }
